c5422952610c55e30cb21ff410d50035b83c2a13
Jakub Głuszecki <jakub.gluszecki@expander.pl>
2012-07-11 12:56:09 +0200

    tests

--- test/action_view_extensions/table_helper_test.rb
+++ test/action_view_extensions/table_helper_test.rb
@@ -1,4 +1,8 @@
-require 'test_helper'
+if __FILE__== $0
+  require '../test_helper'
+else
+  require 'test_helper'
+end
 
 class TableHelperTest < ActionView::TestCase
 
@@ -8,13 +12,23 @@ class TableHelperTest < ActionView::TestCase
     end
 
     should "have 3 rows" do
-      concat table_for(@collection) do |f|
+      concat(table_for(@collection) do |t|
 
-      end
+      end)
 
       rows = css_select 'table tbody tr'
       assert_equal 3, rows.size
 
+    end
+
+    should "have 2 columns" do
+      concat(table_for(@collection) do |t|
+        t.column :downcase
+        t.column :upcase
+      end)
+
+      cols = css_select 'table tbody tr:first-child td'
+      assert_equal 2, cols.size
     end
   end
 end
