49c1145070fcc0932b1adfc46ff00181dc1d54cd
Mike Rogers <me@mikerogers.io>
2018-09-19 16:38:20 +0100

    Fallback for if @ops is not set

--- lib/easy_table/components/base.rb
+++ lib/easy_table/components/base.rb
@@ -2,6 +2,7 @@ module EasyTable
   module Components
     module Base
       def translate(key)
+        @opts ||= {}
         controller = @template.controller_name
         I18n.t("easy_table.#{controller.singularize}.#{key}", default: (@opts[:_title_human_attribute_name] || key.to_s))
       end
